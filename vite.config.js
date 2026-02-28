df7a3270c79086bf832a2131b4c20d75de4e3d21
adorniram <poissonzerty@gmail.com>
2026-02-28 19:38:57 +0000

    fix: change base path to root for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // Set `base` to your repository name when deploying to GitHub Pages
-  // e.g. base: '/REPO_NAME/', replace below placeholder
-  base: '/<REPO_NAME>/',
+  base: '/',
   plugins: [react()],
   server: {
-    historyApiFallback: false
+    historyApiFallback: true
   }
 })
